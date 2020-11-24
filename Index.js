9e6009dbf804aac55b121ca5cea7e717ffb78fd1
ujjwalpandeyjava <ujjwalpaney.aps@gmail.com>
2020-11-24 12:36:52 +0530

    commit

--- Index.js
+++ Index.js
@@ -1,8 +1,18 @@
+
+var ctx, canvas, ballX = 50, ballY = 50;
 window.onload = function () {
-
-
-    var canvas = document.getElementById('gameCanvas');
-    var canvasContext = canvas.getContext('2d')
-    canvasContext.fillStyle = '#0e0e0f'
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    drawEverything();
+}
+function drawEverything(ctx) {
+    canvas = document.getElementById('gameCanvas');
+    ctx = canvas.getContext('2d');
+    //canvas
+    ctx.fillStyle = '#0e0e0f';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // console.log("Draw fired.");
+    //Ball
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = "RED"
+    ctx.fill();
 }
