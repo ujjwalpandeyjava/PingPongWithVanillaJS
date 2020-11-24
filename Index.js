730c94feda20b28888574fb62722a30944f447b7
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2020-11-24 14:11:17 +0530

    commit

--- Index.js
+++ Index.js
@@ -1,10 +1,27 @@
 
-var ctx, canvas, ballX = 50, ballY = 50;
+var ctx, canvas, ballX = 50, ballY = 50, changeInballX = 15, changeInBallY = 10;
+var audio1 = document.getElementById("myAudio1");
 window.onload = function () {
-    setInterval(drawEverything, 200);
+    let framesPerSec = 60;
+    setInterval(() => {
+        movingThings();
+        drawEverything();
+
+    }, 1000 / framesPerSec);
+}
+function movingThings() {
+    ballX += changeInballX;
+    ballY += changeInBallY;
+    if (ballX >= 900 || ballX <= 0) {
+        audio1.play();
+        changeInballX *= -1;
+    }
+    if (ballY >= 700 || ballY <= 0) {
+        changeInBallY *= -1;
+    }
 }
 function drawEverything(ctx) {
-    ballX += 5
+
     canvas = document.getElementById('gameCanvas');
     ctx = canvas.getContext('2d');
     //canvas
@@ -16,4 +33,11 @@ function drawEverything(ctx) {
     ctx.arc(ballX, ballY, 7, 0, 2 * Math.PI);
     ctx.fillStyle = "RED"
     ctx.fill();
+    //Paddle Left
+
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 100, 10, 100)
+    //Paddle Right
+    ctx.fillStyle = 'white'
+    ctx.fillRect(canvas.width - 10, 200, 10, 100)
 }
