ce1b0cdeef4c88ab66f449937b2cb0df1ba389bf
ujjwalpandeyjava <ujjwalpaney.aps@gmail.com>
2020-11-24 10:30:42 +0530

    commit

---
+++ Index.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('gameCanvas');
+var canvasContext = canvas.getContext('2d')
+canvasContext.fillStyle = '#0e0e0f'
+canvasContext.fillRect(0, 0, canvas.width, canvas.height)
